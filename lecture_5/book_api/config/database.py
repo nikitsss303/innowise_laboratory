e40baf695e791cb4ecba71682e0173ac1e72becd
nikitsss303 <nikita.a.smantser@gmail.com>
2025-12-10 13:23:16 +0300

    Connect to db

---
+++ lecture_5/book_api/config/database.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+
+
+load_dotenv(".env")
+DB_DRIVER= os.getenv("DB_DRIVER")
+DB_USER= os.getenv("DB_USER")
+DB_NAME= os.getenv("DB_NAME")
+DB_HOST= os.getenv("DB_HOST")
+DB_PORT= os.getenv("DB_PORT")
+DB_PASSWORD= os.getenv("DB_PASSWORD")
+
+SQLALCHEMY_DATABASE_URL = f"{DB_DRIVER}:///{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
