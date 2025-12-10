49f4b1d0a9bff45dc275ee21f075a9e4e671db47
nikitsss303 <nikita.a.smantser@gmail.com>
2025-12-10 14:50:35 +0300

    Add Book schemas

---
+++ lecture_5/book_api/schemas/book.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class BaseBook(BaseModel):
+    title: str
+    author: str
+    year: Optional[str] = None
+
+class ReadBook(BaseBook):
+    class Config:
+        from_attributes = True
+
+class CreateBook(BaseBook):
+    pass
+
+class UpdateBook(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    year: Optional[str] = None
+
+class DeleteBook(BaseModel):
+    id: int
