f79920e29a0e624e4b2967724c812e1244b6b740
nikitsss303 <nikita.a.smantser@gmail.com>
2025-12-11 23:30:57 +0300

    Write app enter point

--- lecture_5/main.py
+++ lecture_5/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from lecture-5!")
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import uvicorn
+
+from book_api.init_db import init_models 
+from book_api.routers.api import router as api_router
+
+
+@asynccontextmanager
+async def lifespan(book_api: FastAPI):
+    await init_models()
+    yield
+
+book_api = FastAPI(title="Book API", version="1.0", lifespan= lifespan)
+
+
+book_api.include_router(api_router)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "main:book_api",
+        host="127.0.0.1",   
+        port=8000,          
+        reload=True,        
+    )
